26bd4c1cbb750dda6439d69eaf1b14ebd30e7809
felipehdev <felipehdev@gmail.com>
2022-07-28 09:38:17 -0300

    first commit

---
+++ src/assets/componentes/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import S from './Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <div className={S.navbar}>
+        <h1>titulo</h1>
+
+    </div>
+  )
+}
+
+export default Navbar
